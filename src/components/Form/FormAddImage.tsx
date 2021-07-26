5ac8a572963dda30f09c9836bc54cf29802158db
marcoskloss <marcoskloss2001@gmail.com>
2021-07-26 18:35:33 -0300

    initial commit

---
+++ src/components/Form/FormAddImage.tsx
@@ -0,0 +1,106 @@
+import { Box, Button, Stack, useToast } from '@chakra-ui/react';
+import { useForm } from 'react-hook-form';
+import { useState } from 'react';
+import { useMutation, useQueryClient } from 'react-query';
+
+import { api } from '../../services/api';
+import { FileInput } from '../Input/FileInput';
+import { TextInput } from '../Input/TextInput';
+
+interface FormAddImageProps {
+  closeModal: () => void;
+}
+
+const MAX_FILE_SIZE = 1000000;
+
+export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
+  const [imageUrl, setImageUrl] = useState('');
+  const [localImageUrl, setLocalImageUrl] = useState('');
+  const toast = useToast();
+
+  const formValidations = {
+    image: {
+      required: 'Arquivo obrigatório',
+      validate: {
+        lessThan10MB: fileList =>
+          fileList[0].size < MAX_FILE_SIZE ||
+          'O arquivo deve ser menor que 10MB',
+        acceptedFormats: v =>
+          /image\/(jpeg|png|gif)/.test(v[0].type) ||
+          'Somente são aceitos arquivos PNG, JPEG e GIF',
+      },
+    },
+    title: {
+      required: 'Título obrigatório',
+      minLength: 'Mínimo de 2 caracteres',
+      maxLength: 'Máximo de 20 caracteres',
+    },
+    description: {
+      required: 'Descrição obrigatória',
+      maxLength: 'Máximo de 65 caracteres',
+    },
+  };
+
+  const queryClient = useQueryClient();
+  const mutation = useMutation(
+    // TODO MUTATION API POST REQUEST,
+    {
+      // TODO ONSUCCESS MUTATION
+    }
+  );
+
+  const { register, handleSubmit, reset, formState, setError, trigger } =
+    useForm();
+  const { errors } = formState;
+
+  const onSubmit = async (data: Record<string, unknown>): Promise<void> => {
+    try {
+      // TODO SHOW ERROR TOAST IF IMAGE URL DOES NOT EXISTS
+      // TODO EXECUTE ASYNC MUTATION
+      // TODO SHOW SUCCESS TOAST
+    } catch {
+      // TODO SHOW ERROR TOAST IF SUBMIT FAILED
+    } finally {
+      // TODO CLEAN FORM, STATES AND CLOSE MODAL
+    }
+  };
+
+  return (
+    <Box as="form" width="100%" onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={4}>
+        <FileInput
+          setImageUrl={setImageUrl}
+          localImageUrl={localImageUrl}
+          setLocalImageUrl={setLocalImageUrl}
+          setError={setError}
+          trigger={trigger}
+          error={errors.file}
+          {...register('file', formValidations.image)}
+        />
+
+        <TextInput
+          placeholder="Título da imagem..."
+          error={errors.title}
+          {...register('title', formValidations.title)}
+        />
+
+        <TextInput
+          placeholder="Descrição da imagem..."
+          error={errors.description}
+          {...register('description', formValidations.description)}
+        />
+      </Stack>
+
+      <Button
+        my={6}
+        isLoading={formState.isSubmitting}
+        isDisabled={formState.isSubmitting}
+        type="submit"
+        w="100%"
+        py={6}
+      >
+        Enviar
+      </Button>
+    </Box>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,49 @@
+import { Button, Box } from '@chakra-ui/react';
+import { useMemo } from 'react';
+import { useInfiniteQuery } from 'react-query';
+
+import { Header } from '../components/Header';
+import { Card, CardList } from '../components/CardList';
+import { api } from '../services/api';
+import { Loading } from '../components/Loading';
+import { Error } from '../components/Error';
+
+export default function Home(): JSX.Element {
+  const getImages = async ({ pageParam = null }): Promise<any> => {
+    const response = await api.get('/images/', {
+      params: { pageParam }
+    });
+  };
+
+  const {
+    data,
+    isLoading,
+    isError,
+    isFetchingNextPage,
+    fetchNextPage,
+    hasNextPage,
+  } = useInfiniteQuery('images', getImages, {
+    getNextPageParam: (...args) => {
+
+    },
+  });
+
+  const formattedData = useMemo(() => {
+
+  }, [data]);
+
+  // TODO RENDER LOADING SCREEN
+
+  // TODO RENDER ERROR SCREEN
+
+  return (
+    <>
+      <Header />
+
+      <Box maxW={1120} px={20} mx="auto" my={20}>
+        <CardList cards={formattedData} />
+        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+      </Box>
+    </>
+  );
+}
